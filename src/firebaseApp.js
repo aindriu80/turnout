9e45bb8ae01fe2d6b8135e8447c2b7c0813bf260
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-11-07 21:15:20 +0000

    Intro to firebase, redirects to login component

---
+++ src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyABhMgsQ-cVVXt57HXJVebAjpUVPOjHYs4",
+  authDomain: "turnout-1396a.firebaseapp.com",
+  databaseURL: "https://turnout-1396a.firebaseio.com",
+  projectId: "turnout-1396a",
+  storageBucket: "turnout-1396a.appspot.com",
+  messagingSenderId: "390396090430"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
