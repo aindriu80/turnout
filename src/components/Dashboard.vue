acc2e3a27e7a36d1ce2963ecad4acd2b2ee334ae
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-11-13 20:51:59 +0000

    Adding a realtime database

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import { firebaseApp } from "../firebaseApp";
+import { firebaseApp, eventsRef } from "../firebaseApp";
 import AddEvent from "./AddEvent.vue";
 export default {
   methods: {
@@ -21,6 +21,15 @@ export default {
   },
   components: {
     AddEvent
+  },
+  mounted() {
+    eventsRef.on("value", snap => {
+      let events = [];
+      snap.forEach(event => {
+        events.push(event.val());
+      });
+      console.log("events", events);
+    });
   }
 };
 </script>
